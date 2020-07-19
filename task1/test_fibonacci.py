0df8e88e42b7a2d9559afcb1fb5c0c91fa21c98b
AbanoubMethany <abanoub-harby-marzouk.methany@hsrw.org>
2020-07-19 04:55:46 +0200

    check if test fails

--- task1/test_fibonacci.py
+++ task1/test_fibonacci.py
@@ -4,7 +4,7 @@ import unittest
 class TestFibonadfacci (unittest.TestCase):
     
     def test_fib(self):
-        self.assertEqual(fib.fib(2), 1)
+        self.assertEqual(fib.fib(2), 0)
         self.assertEqual(fib.fib(5), 5)
         self.assertEqual(fib.fib(12), 144)
         
