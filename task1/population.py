daa7104b3904c114f20aeff943b5e99bd5831673
AbanoubMethany <abanoub-harby-marzouk.methany@hsrw.org>
2020-07-22 22:11:19 +0200

    add test cases

--- task1/population.py
+++ task1/population.py
@@ -11,3 +11,18 @@ df.rename(columns={'year':'country'},inplace=True)
 df.set_index('country', inplace=True)
 
 
+
+
+def test_row():
+    """Check row number in df."""
+    assert len(df) == 225
+
+
+def test_column():
+    """Check column number in df."""
+    assert len(df.columns) == 31
+
+
+def test_population():
+    """Check sum of '2010' column in df."""
+    assert round(df['2010'].sum()) == 7065
