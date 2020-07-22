270c498847765d73d54ac0a0a05703f3a38ace43
AbanoubMethany <abanoub-harby-marzouk.methany@hsrw.org>
2020-07-22 22:10:22 +0200

    read data to df

--- task1/population.py
+++ task1/population.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
 
+
 df = pd.read_csv('input.txt', sep=',', escapechar='\\',
-                 index_col=0, encoding="utf-16")
-print(df)
-fhgaewhkahfskdf
+                  encoding="utf-16")
+df['2010'] = pd.to_numeric(df['2010'], errors='coerce')
+
+print(df['2010'].sum())
+df.rename(columns={'year':'country'},inplace=True)
+df.set_index('country', inplace=True)
+
+
